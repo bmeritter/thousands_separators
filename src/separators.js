13b0e13dbe7d81b19bcdab71e7223afcf82da7b3
bmeritter <412018745@qq.com>
2016-05-18 23:43:05 +0800

    jasmine success

--- src/separators.js
+++ src/separators.js
@@ -1,16 +1,31 @@
 'use strict';
 
-function thousands_separators(num) 
+function thousands_separators(num)
 {
- 	var list = new String(num).split('').reverse();
-   	for(var i = 0; i < list.length; i++)
-	{
-       		if(i % 4 == 3)
-		{
-         		list.splice(i, 0, ',');
-       		}
-   	}
-   	return list.reverse().join(''); 
+
+    var num = (num || 0).toString(), temp;
+    var i = num.indexOf(".");
+    var str, s = num;
+    if (i != -1) {
+        num = num.substring(0,i);
+        str = s.substring(i,s.length);
+    }
+
+    temp = num.length % 3;
+    switch (temp) {
+        case 1:
+            num = '00' + num;
+            break;
+        case 2:
+            num = '0' + num;
+            break;
+    }
+    num =  num.match(/\d{3}/g).join(',').replace(/^0+/, '');
+    if (i != -1) {
+       num = num.concat(str);
+    }
+
+    return num;
 }
 
 module.exports = thousands_separators;
